f412b43bd3d473400f5ce240335b2a2aea87b529
Thouhid98 <thouhidhussain98@gmail.com>
2025-05-08 19:15:31 +0600

    TypeScript Assignment

---
+++ module1/src/assignment.ts
@@ -0,0 +1,121 @@
+function formatString(input: string, toUpper?: boolean): string {
+  if (toUpper === false) {
+    return input.toLowerCase();
+  }
+  return input.toUpperCase();
+}
+
+function filterByRating(
+  items: { title: string; rating: number }[]
+): { title: string; rating: number }[] {
+  return items.filter(function (item) {
+    return item.rating >= 4;
+  });
+}
+const books = [
+  { title: "Book A", rating: 4.5 },
+  { title: "Book B", rating: 3.2 },
+  { title: "Book C", rating: 5.0 },
+];
+
+function concatenateArrays<T>(...arrays: T[][]): T[] {
+  let result: T[] = [];
+  for (let i = 0; i < arrays.length; i++) {
+    let currentArray = arrays[i];
+    for (let j = 0; j < currentArray.length; j++) {
+      result.push(currentArray[j]);
+    }
+  }
+  return result;
+}
+
+class Vehicle {
+  private make: string;
+  private year: number;
+
+  constructor(make: string, year: number) {
+    this.make = make;
+    this.year = year;
+  }
+
+  public getInfo(): string {
+    return `Make: ${this.make}, Year:${this.year}`;
+  }
+}
+
+class Car extends Vehicle {
+  private model: string;
+
+  constructor(make: string, year: number, model: string) {
+    super(make, year);
+    this.model = model;
+  }
+  public getModel(): string {
+    return `Model: ${this.model}`;
+  }
+}
+
+const myCar = new Car("Toyota", 2020, "Corolla");
+
+function processValue(value: string | number): number {
+  if (typeof value === "string") {
+    return value.length;
+  } else {
+    return value * 2;
+  }
+}
+
+interface Product {
+  name: string;
+  price: number;
+}
+
+function getMostExpensiveProduct(products: Product[]): Product | null {
+  if (products.length === 0) {
+    return null;
+  }
+  return products.reduce((maxProduct, currentProduct) => {
+    if (maxProduct > currentProduct) {
+      return maxProduct;
+    } else {
+      return currentProduct;
+    }
+  });
+}
+
+const products = [
+  { name: "Pen", price: 10 },
+  { name: "Notebook", price: 25 },
+  { name: "Bag", price: 50 },
+  { name: "Bagg", price: 100 },
+];
+
+enum Day {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday,
+}
+
+function getDayType(day: Day): string {
+  if (day === Day.Saturday || day === Day.Sunday) {
+    return "Weekend";
+  } else {
+    return "Weekday";
+  }
+}
+
+async function squareAsync(n: number): Promise<number> {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (n < 0) {
+        reject(new Error("Negative number not allowed"));
+      } else {
+        resolve(n * n);
+      }
+    }, 1000);
+  });
+}
